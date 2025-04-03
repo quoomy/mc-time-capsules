48994a48f57b28fde1fe1f3f0e33fce636cfa788
quoomy <git@quoomy.com>
2025-04-03 16:41:01 +0900

    Basic fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Build setup test');
+import fastify from 'fastify';
+
+const app = fastify();
+
+async function startServer() {
+	app.ready();
+	try {
+		await app.listen({ port: 4242, host: '0.0.0.0' });
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+}
+startServer();
+
+app.get('/', async (request, reply) => {
+	return { hello: 'world' };
+});
